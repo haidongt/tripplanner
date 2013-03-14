6afd37558627d64b869095d54bc6b69ea94a0749
Haidong Tang <thd.thu@gmail.com>
2013-03-14 00:14:34 -0500

    updated data models

--- scriptForData.py
+++ scriptForData.py
@@ -1,13 +1,11 @@
 from tripDB.models import Destination
 
 file = open("data.txt")
-count = 0
 while 1:
-    count = count + 1
     line = file.readline()
     if not line:
         break
     line = line.strip()
     fields = line.split("*")
-    newRow = Destination(dest_ID = count, name = fields[0], location = fields[2]+","+fields[3], description = fields[1])
+    newRow = Destination(name = fields[0], location = fields[2]+","+fields[3], description = fields[1])
     newRow.save()
--- tripDB/models.py
+++ tripDB/models.py
@@ -3,14 +3,19 @@ from django.db import models
 # Create your models here.
 
 class Destination(models.Model):
-    dest_ID = models.IntegerField(default=0)
+    #dest_ID = models.IntegerField(default=0)
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
     
 class Route(models.Model):
-    route_ID = models.IntegerField(default=0)
-    dest_A_ID = models.IntegerField(default=0)
-    dest_B_ID = models.IntegerField(default=0)
+    #route_ID = models.IntegerField(default=0)
+    #dest_A_ID = models.IntegerField(default=0)
+    #dest_B_ID = models.IntegerField(default=0)
+    destA = models.ForeignKey(Destination, related_name='route_from')
+    destB = models.ForeignKey(Destination, related_name='route_to')
     driving_time = models.IntegerField(default=0)
-    
+
