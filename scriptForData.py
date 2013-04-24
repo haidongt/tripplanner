81a9d9d4a5db2b415844ef5374d089c380c9ab13
Haidong Tang <thd.thu@gmail.com>
2013-04-24 00:28:57 -0500

    inserted data

--- scriptForData.py
+++ scriptForData.py
@@ -1,45 +1,43 @@
 from tripDB.models import Destination, Route
 
-file = open("data.txt")
+file = open("data/np_out.txt")
 while 1:
     line = file.readline()
     if not line:
         break
     line = line.strip()
     fields = line.split("*")
-    newRow = Destination(name = fields[0], location = fields[2]+","+fields[3], description = fields[1])
+    newRow = Destination(name = fields[0], location = fields[2]+","+fields[3], tag = "np")
     newRow.save()
 
-yellowstone = Destination.objects.filter(name__icontains="yellowstone")[0]
-rainier = Destination.objects.filter(name__icontains="rainier")[0]
-grandteton = Destination.objects.filter(name__icontains="grand teton")[0]
-yosemite = Destination.objects.filter(name__icontains="yosemite")[0]
 
-newRow = Route(destA = yellowstone, destB = grandteton, driving_time = 2)
-newRow.save()
-newRow = Route(destA = yellowstone, destB = yosemite, driving_time = 12)
-newRow.save()
-newRow = Route(destA = yellowstone, destB = rainier, driving_time = 18)
-newRow.save()
-'''
-#join
-yellowstone_yosemite = Route.objects.filter(destA__name__icontains="yellowstone", destB__name__icontains="yosemite")
-yellowstone_yosemite[0].driving_time
+file = open("data/zoo_out.txt")
+while 1:
+    line = file.readline()
+    if not line:
+        break
+    line = line.strip()
+    fields = line.split("*")
+    newRow = Destination(name = fields[0], location = fields[2]+","+fields[3], tag = "zoo")
+    newRow.save()
+	
+	
+file = open("data/museums_out.txt")
+while 1:
+    line = file.readline()
+    if not line:
+        break
+    line = line.strip()
+    fields = line.split("*")
+    newRow = Destination(name = fields[0], location = fields[2]+","+fields[3], tag = "museum")
+    newRow.save()
 
-#update
-yellowstone.description = "lalala"
-yellowstone.save()
-yellowstone = Destination.objects.filter(name__icontains="yellowstone")[0]
-yellowstone.description
-yellowstone.description = 'Yellowstone National Park, WY 82190, USA'
-yellowstone.save()
-
-#insertion and deletion
-uiuc = Destination.objects.get(name="UIUC")
-uiuc = Destination(name = "UIUC", location = "123,234", description = "University of Illinois at Urbana-Champaign")
-uiuc = Destination.objects.get(name="UIUC")
-uiuc.save()
-uiuc = Destination.objects.get(name="UIUC")
-uiuc.delete()
-uiuc = Destination.objects.get(name="UIUC")
-'''
+file = open("data/city_out.txt")
+while 1:
+    line = file.readline()
+    if not line:
+        break
+    line = line.strip()
+    fields = line.split("*")
+    newRow = Destination(name = fields[0], location = fields[2]+","+fields[3], tag = "city")
+    newRow.save()
